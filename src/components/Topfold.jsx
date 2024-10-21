80d3b9561b31c9312fac1f46482381add7722eaa
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-21 22:46:44 +0630

    feat: done topfold design

--- src/components/Topfold.jsx
+++ src/components/Topfold.jsx
@@ -1,14 +1,23 @@
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faAdd, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Topfold = () => {
   return (
     <div className="flex justify-between m-auto w-1/2 mt-3">
-      <div>
+      <div className="p-2 rounded-md border bg-white">
         <FontAwesomeIcon icon={faSearch} />
-        <input type="text" placeholder="Search your expenses..." />
+        <input
+          type="text"
+          placeholder="Search your expenses..."
+          className="ml-2 border-none outline-none"
+        />
       </div>
-      <div>Add New</div>
+      <div>
+        <button className="pl-2 pr-2 pt-1 pb-1 rounded-md border-black border-2 shadow-lg">
+          <FontAwesomeIcon icon={faAdd} className="ml-1 mr-1" />
+          Add New
+        </button>
+      </div>
     </div>
   );
 };
