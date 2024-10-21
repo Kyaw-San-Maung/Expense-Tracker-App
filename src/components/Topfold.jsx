3577d0a821b1e1a0e1f9d48830ed592938d8dc49
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-21 21:01:12 +0630

    chore: integrate with fontawsome

--- src/components/Topfold.jsx
+++ src/components/Topfold.jsx
@@ -1,7 +1,13 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Topfold = () => {
   return (
     <div className="flex justify-between m-auto w-1/2 mt-3">
-      <div>Search</div>
+      <div>
+        <FontAwesomeIcon icon={faSearch} />
+        <input type="text" placeholder="Search your expenses..." />
+      </div>
       <div>Add New</div>
     </div>
   );
