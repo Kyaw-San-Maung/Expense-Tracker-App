2c2106605ae441e849cfc31c65746804e9dbb4f9
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-22 00:48:45 +0630

    feat: add expenseList style

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,5 +1,9 @@
 const ExpenseList = () => {
-  return <div>ExpenseList</div>;
+  return (
+    <div className="flex justify-between m-auto w-1/2 mt-3">
+      <h2>ExpensesList</h2>
+    </div>
+  );
 };
 
 export default ExpenseList;
