01fd6e1272a5b1708d18f544eae5b6ce75bfa345
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-22 00:31:53 +0630

    feat: add topfold input function

--- src/components/Topfold.jsx
+++ src/components/Topfold.jsx
@@ -1,15 +1,26 @@
 import { faAdd, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const Topfold = () => {
+  const [query, setQuery] = useState("");
+
+  const inputHandler = (e) => {
+    const inputValue = e.target.value;
+    setQuery(inputValue);
+    console.log(inputValue);
+  };
+
   return (
     <div className="flex justify-between m-auto w-1/2 mt-3">
       <div className="p-2 rounded-md border bg-white">
         <FontAwesomeIcon icon={faSearch} />
         <input
+          value={query}
           type="text"
           placeholder="Search your expenses..."
           className="ml-2 border-none outline-none"
+          onChange={(e) => inputHandler(e)}
         />
       </div>
       <div>
