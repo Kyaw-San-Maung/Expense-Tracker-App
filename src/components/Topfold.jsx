7185041cd4b1a839e46dd8d686e89df7ee2f1ea8
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-22 10:09:23 +0630

    feat: add other topfold

--- src/components/Topfold.jsx
+++ src/components/Topfold.jsx
@@ -13,22 +13,30 @@ const Topfold = () => {
 
   return (
     <div className="flex justify-between m-auto w-1/2 mt-3">
-      <div className="p-2 rounded-md border bg-white">
-        <FontAwesomeIcon icon={faSearch} />
-        <input
-          value={query}
-          type="text"
-          placeholder="Search your expenses..."
-          className="ml-2 border-none outline-none"
-          onChange={(e) => inputHandler(e)}
-        />
-      </div>
-      <div>
-        <button className="pl-2 pr-2 pt-1 pb-1 rounded-md border-black border-2 shadow-lg">
-          <FontAwesomeIcon icon={faAdd} className="ml-1 mr-1" />
-          Add New
-        </button>
-      </div>
+      {window.location.pathname == "/" ? (
+        <>
+          <div className="p-2 rounded-md border bg-white">
+            <FontAwesomeIcon icon={faSearch} />
+            <input
+              value={query}
+              type="text"
+              placeholder="Search your expenses..."
+              className="ml-2 border-none outline-none"
+              onChange={(e) => inputHandler(e)}
+            />
+          </div>
+          <div>
+            <button className="pl-2 pr-2 pt-1 pb-1 rounded-md border-black border-2 shadow-lg">
+              <FontAwesomeIcon icon={faAdd} className="ml-1 mr-1" />
+              Add New
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>Other Topfold</div>
+        </>
+      )}
     </div>
   );
 };
