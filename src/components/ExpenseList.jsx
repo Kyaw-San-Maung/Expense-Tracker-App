53ccba36ae9d57578a4b15863d092ba10fd8cb39
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-23 11:30:12 +0630

    feat: create expenslist card

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,7 +1,31 @@
 const ExpenseList = () => {
+  const list = [
+    {
+      title: "Make a purchase",
+      logo: "F&B",
+      createdAt: Date.now(),
+      amount: 1234,
+    },
+    {
+      title: "Make a purchase",
+      logo: "F&B",
+      createdAt: Date.now(),
+      amount: 1234,
+    },
+  ];
   return (
     <div className="flex justify-between m-auto w-1/2 mt-3">
-      <h2>ExpensesList</h2>
+      <div>
+        {list.length ? (
+          list.map((item) => (
+            <>
+              <h3>{item.title}</h3>
+            </>
+          ))
+        ) : (
+          <>Nothing in the List</>
+        )}
+      </div>
     </div>
   );
 };
