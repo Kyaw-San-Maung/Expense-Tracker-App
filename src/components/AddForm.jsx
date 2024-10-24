201accb0b43550ab2f25f996f5315d8937371c77
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-24 11:52:35 +0630

    feat: create Add Form for Expense

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,10 +1,30 @@
-import { Input } from "@nextui-org/react";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+} from "@nextui-org/react";
 
 const AddForm = () => {
   return (
-    <div>
-      <label>Email</label>
-      <Input type="email" label="Email" />
+    <div className="m-auto w-1/2 mt-3">
+      <div className="p-4 mt-6 border">
+        <Input size="sm" label="Title" className="mb-3" />
+
+        <Input size="sm" label="Amount" className="mb-3" />
+        <Dropdown>
+          <DropdownTrigger>
+            <Button variant="bordered">Type</Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+            <DropdownItem key="new">F&B</DropdownItem>
+            <DropdownItem key="copy">Shopping</DropdownItem>
+            <DropdownItem key="edit">Family & Friends</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </div>
   );
 };
