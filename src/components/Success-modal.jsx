0d03da4356aebc3544bda4f47ae2b0247ac78420
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-10-28 10:33:50 +0630

    feat: add Modal for success add expense

---
+++ src/components/Success-modal.jsx
@@ -0,0 +1,18 @@
+import Modal from "react-modal";
+
+const Successmodal = () => {
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  return <Modal></Modal>;
+};
+
+export default Successmodal;
